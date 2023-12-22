32dc7a3b00282105d4d921f9fa8638172912693e
lamthehuy08 <thehuy161802@gmail.com>
2023-12-22 09:52:31 +0700

    update

--- HomeWork07/lethehuy_20204566/PMChamCong/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module main.pmchamcong {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens main.pmchamcong to javafx.fxml;
-    exports main.pmchamcong;
-}
